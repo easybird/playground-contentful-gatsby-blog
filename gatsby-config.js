053598b968ee99213bc5ac3102933fb03ba960c1
Jonathan Dierckens <jonathan.dierckens@inthepocket.mobi>
2018-03-19 11:37:09 +0100

    link gatsby to contentful basic Post blog info: title, slug, tags, body

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,19 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
-};
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+  ],
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,47 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+const IndexPage = ({ data: { allContentfulPost: { edges } } }) => {
+  return (
+    <div>
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      {edges.map(({ node }) => {
+        return (
+          <div>
+            <h4>{node.title.title}</h4>
+            <h5>{node.slug}</h5>
+            {node.tags &&
+              node.tags.map(tag => <Link to={'/' + tag}>{tag}</Link>)}
+            <p>{node.body.body}</p>
+          </div>
+        )
+      })}
+      <p>{JSON.stringify(edges)}</p>
+      <Link to="/page-2/">Go to page 2</Link>
+    </div>
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+  query BasicContent {
+    allContentfulPost {
+      edges {
+        node {
+          id
+          title {
+            title
+          }
+          body {
+            body
+          }
+          slug
+          tags
+        }
+      }
+    }
+  }
+`

c340065a91ca1064d80f0a2d960cf24ee4c44413
Jonathan Dierckens <jonathan.dierckens@inthepocket.mobi>
2018-03-19 20:08:00 +0100

    create basic index page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './index.css'
 
 const Header = () => (
   <div
     style={{
-      background: 'rebeccapurple',
+      background: 'lightseagreen',
       marginBottom: '1.45rem',
     }}
   >
@@ -23,9 +24,20 @@ const Header = () => (
             textDecoration: 'none',
           }}
         >
-          Gatsby
+          Proof Of Concept: Basic Blog
         </Link>
       </h1>
+      <div className="header">
+        <Link className="header-item" to="/authors">
+          <h5>Authors</h5>
+        </Link>
+        <Link className="header-item" to="/categories">
+          <h5>Categories</h5>
+        </Link>
+        <Link className="header-item" to="/tags">
+          <h5>Tags</h5>
+        </Link>
+      </div>
     </div>
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,66 @@
 import React from 'react'
-import Link from 'gatsby-link'
+import Link, { navigateTo } from 'gatsby-link'
+import './index.css'
+
+const tagStyle = { marginRight: '8px' }
+
+const createUrlFromSlug = slug => '/posts/' + slug.split(' ').join('-')
 
 const IndexPage = ({ data: { allContentfulPost: { edges } } }) => {
-  return (
-    <div>
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      {edges.map(({ node }) => {
-        return (
-          <div>
-            <h4>{node.title.title}</h4>
-            <h5>{node.slug}</h5>
-            {node.tags &&
-              node.tags.map(tag => <Link to={'/' + tag}>{tag}</Link>)}
-            <p>{node.body.body}</p>
+  const nodes = edges.map(({ node }) => {
+    return (
+      <div
+        className="node"
+        onClick={() => navigateTo(createUrlFromSlug(node.slug))}
+      >
+        <div className="title">
+          <h4>{node.title.title}</h4>
+        </div>
+        {node.featuredImage && (
+          <div className="image">
+            <img src={node.featuredImage.resolutions.src} />
           </div>
-        )
-      })}
-      <p>{JSON.stringify(edges)}</p>
-      <Link to="/page-2/">Go to page 2</Link>
+        )}
+        <div className="info-block">
+          {node.author && (
+            <div className="info">
+              <span className="info-title">Author:</span>
+              <Link to={node.author[0].name}>{node.author[0].name}</Link>
+            </div>
+          )}
+          {node.category && (
+            <div className="info">
+              <span className="info-title">Category:</span>
+              <Link to={node.category[0].title}>{node.category[0].title}</Link>
+            </div>
+          )}
+        </div>
+
+        {node.tags && (
+          <div className="tags">
+            {node.tags.map(tag => (
+              <Link style={tagStyle} to={'/' + tag}>
+                {tag}
+              </Link>
+            ))}
+          </div>
+        )}
+      </div>
+    )
+  })
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+        flexWrap: 'wrap',
+        margin: '50px',
+      }}
+    >
+      {nodes}
+      {nodes}
+      {nodes}
     </div>
   )
 }
@@ -39,7 +80,19 @@ export const query = graphql`
             body
           }
           slug
+          date
           tags
+          author {
+            name
+          }
+          category {
+            title
+          }
+          featuredImage {
+            resolutions {
+              src
+            }
+          }
         }
       }
     }
